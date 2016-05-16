98842521bb2a53ee8833ed06273a06d960277402
Chris <nearman.cj@gmail.com>
2016-05-15 17:15:16 -0700

    Adding test_code and tests.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 // app/index.js
 const express = require('express')  
+const test_counter = require('./test_code.js')
 const app = express()  
 const port = 3000
 
 app.get('/', (request, response) => {  
+	test_counter.increment();
   response.send('Hello from Express!')
 })
 
